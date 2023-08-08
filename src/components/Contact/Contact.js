3c25561a027cde4dae04b73f633441487e9aab9c
Jaskirat <jaskirathsingh2@gmail.com>
2023-08-08 23:39:07 +0530

    contact section don

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {HiOutlineMail} from "react-icons"
+import "../Contact/contact.css"
+import {HiOutlineMail} from "react-icons/hi"
+import {BsWhatsapp} from "react-icons/bs"
 
 const Contact = () => {
   return (
@@ -10,13 +12,26 @@ const Contact = () => {
       <div className='container contactContainer'>
         <div className='contactOptions'>
           <article className='contactOption'>
+            < HiOutlineMail className='contactOptionIcon' />
             <h4>Email</h4>
             <h5>jaskirathsingh2@gmail.com</h5>
-            <a href='jaskirathsingh2@gmail.com'> Send a message  </a>
+            <a href='mailto:jaskirathsingh2@gmail.com'> Send a message  </a>
+          </article>
+         
+          <article className='contactOption'>
+            < BsWhatsapp className='contactOptionIcon' />
+            <h4>Whatsapp</h4>
+            <h5>+91-8800684439</h5>
+            <a href='https://api.whatsapp.com/send?phone=+918800684439'> Send a message </a>
           </article>
         </div>
         {/* End of Contact option */}
-        <form action=''></form>
+        <form action=''>
+          <input type='text' name='name' placeholder='Your full name' required/>
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows={7} placeholder='Your message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send message</button>
+        </form>
       </div>
     </section>
   )
